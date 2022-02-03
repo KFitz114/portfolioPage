cc5caa0e527d71223aad52a919ec59f56184d8a5
KFitz114 <flipperly@protonmail.com>
2022-02-03 12:17:48 -0500

    first commit

---
+++ portfolio.js
@@ -0,0 +1,29 @@
+const card = document.querySelector('.cardInner');
+
+card.addEventListener('mouseenter' , function() {
+    card.classList.toggle('is-flipped');
+});
+
+card.addEventListener('mouseleave' , function() {
+    card.classList.toggle('is-flipped');
+});
+
+const cardOne = document.querySelector('.cardInnerOne');
+
+cardOne.addEventListener('mouseenter' , function() {
+    cardOne.classList.toggle('is-flipped');
+});
+
+cardOne.addEventListener('mouseleave' , function() {
+    cardOne.classList.toggle('is-flipped');
+});
+
+const cardTwo = document.querySelector('.cardInnerTwo');
+
+cardTwo.addEventListener('mouseenter' , function() {
+    cardTwo.classList.toggle('is-flipped');
+}); 
+
+cardTwo.addEventListener('mouseleave' , function() {
+    cardTwo.classList.toggle('is-flipped');
+}); 
